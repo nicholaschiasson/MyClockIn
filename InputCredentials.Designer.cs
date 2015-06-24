5e9be64b2b071196f78cf62861f13eac51c77342
unknown <nchiasson@enhansoft.com>
2015-06-24 14:05:50 -0400

    Add project files

---
+++ InputCredentials.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace MyClockIn
+{
+    partial class InputCredentials
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.DomainTextBox = new System.Windows.Forms.TextBox();
+            this.UsernameTextBox = new System.Windows.Forms.TextBox();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.CancelLoginButton = new System.Windows.Forms.Button();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.ClockingInRadioButton = new System.Windows.Forms.RadioButton();
+            this.ClockingOutRadioButton = new System.Windows.Forms.RadioButton();
+            this.SessionRadioButton = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please login to begin your MyClockIn session.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Domain";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Username";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Password";
+            // 
+            // DomainTextBox
+            // 
+            this.DomainTextBox.Location = new System.Drawing.Point(76, 35);
+            this.DomainTextBox.Name = "DomainTextBox";
+            this.DomainTextBox.Size = new System.Drawing.Size(196, 20);
+            this.DomainTextBox.TabIndex = 7;
+            // 
+            // UsernameTextBox
+            // 
+            this.UsernameTextBox.Location = new System.Drawing.Point(76, 61);
+            this.UsernameTextBox.Name = "UsernameTextBox";
+            this.UsernameTextBox.Size = new System.Drawing.Size(196, 20);
+            this.UsernameTextBox.TabIndex = 0;
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(116, 136);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(75, 23);
+            this.LoginButton.TabIndex = 5;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // CancelLoginButton
+            // 
+            this.CancelLoginButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CancelLoginButton.Location = new System.Drawing.Point(197, 136);
+            this.CancelLoginButton.Name = "CancelLoginButton";
+            this.CancelLoginButton.Size = new System.Drawing.Size(75, 23);
+            this.CancelLoginButton.TabIndex = 6;
+            this.CancelLoginButton.Text = "Cancel";
+            this.CancelLoginButton.UseVisualStyleBackColor = true;
+            this.CancelLoginButton.Click += new System.EventHandler(this.CancelLoginButton_Click);
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(76, 87);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(196, 20);
+            this.PasswordTextBox.TabIndex = 1;
+            this.PasswordTextBox.UseSystemPasswordChar = true;
+            // 
+            // ClockingInRadioButton
+            // 
+            this.ClockingInRadioButton.AutoSize = true;
+            this.ClockingInRadioButton.Location = new System.Drawing.Point(108, 113);
+            this.ClockingInRadioButton.Name = "ClockingInRadioButton";
+            this.ClockingInRadioButton.Size = new System.Drawing.Size(75, 17);
+            this.ClockingInRadioButton.TabIndex = 3;
+            this.ClockingInRadioButton.Text = "ClockingIn";
+            this.ClockingInRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // ClockingOutRadioButton
+            // 
+            this.ClockingOutRadioButton.AutoSize = true;
+            this.ClockingOutRadioButton.Location = new System.Drawing.Point(189, 113);
+            this.ClockingOutRadioButton.Name = "ClockingOutRadioButton";
+            this.ClockingOutRadioButton.Size = new System.Drawing.Size(83, 17);
+            this.ClockingOutRadioButton.TabIndex = 4;
+            this.ClockingOutRadioButton.Text = "ClockingOut";
+            this.ClockingOutRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // SessionRadioButton
+            // 
+            this.SessionRadioButton.AutoSize = true;
+            this.SessionRadioButton.Checked = true;
+            this.SessionRadioButton.Location = new System.Drawing.Point(40, 113);
+            this.SessionRadioButton.Name = "SessionRadioButton";
+            this.SessionRadioButton.Size = new System.Drawing.Size(62, 17);
+            this.SessionRadioButton.TabIndex = 2;
+            this.SessionRadioButton.TabStop = true;
+            this.SessionRadioButton.Text = "Session";
+            this.SessionRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // InputCredentials
+            // 
+            this.AcceptButton = this.LoginButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.CancelLoginButton;
+            this.ClientSize = new System.Drawing.Size(284, 171);
+            this.ControlBox = false;
+            this.Controls.Add(this.SessionRadioButton);
+            this.Controls.Add(this.ClockingOutRadioButton);
+            this.Controls.Add(this.ClockingInRadioButton);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.CancelLoginButton);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.UsernameTextBox);
+            this.Controls.Add(this.DomainTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "InputCredentials";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MyClockIn Login";
+            this.Load += new System.EventHandler(this.InputCredentials_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox DomainTextBox;
+        private System.Windows.Forms.TextBox UsernameTextBox;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.Button CancelLoginButton;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.RadioButton ClockingInRadioButton;
+        private System.Windows.Forms.RadioButton ClockingOutRadioButton;
+        private System.Windows.Forms.RadioButton SessionRadioButton;
+    }
+}
---
+++ InputCredentials.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IWshRuntimeLibrary;
+
+namespace MyClockIn
+{
+    public partial class InputCredentials : Form
+    {
+        string SystemAppData;
+        string ProductSystemAppDataFolder;
+        DirectoryInfo SystemDir;
+        string SystemClockInFileName;
+        string SystemClockOutFileName;
+        StreamWriter SystemClockInFile;
+        StreamWriter SystemClockOutFile;
+
+        WrapperImpersonationContext UserContext;
+        string UserAppData;
+        string ProductUserAppDataFolder;
+        DirectoryInfo UserDir;
+        string UserClockInFileName;
+        string UserClockOutFileName;
+        StreamWriter UserClockInFile;
+        StreamWriter UserClockOutFile;
+
+        public InputCredentials()
+        {
+            InitializeComponent();
+        }
+
+        private void InputCredentials_Load(object sender, EventArgs e)
+        {
+            DomainTextBox.Text = Environment.MachineName;
+
+            string StartupFolder = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+            WshShell Shell = new WshShell();
+            string ShortcutAddress = StartupFolder + "\\" + Application.ProductName + ".lnk";
+            IWshShortcut Shortcut = (IWshShortcut)Shell.CreateShortcut(ShortcutAddress);
+            Shortcut.Description = "A startup shortcut. If you delete this shortcut from your computer, LaunchOnStartup.exe will not launch on Windows Startup"; // set the description of the shortcut
+            Shortcut.WorkingDirectory = Application.StartupPath; // working directory
+            Shortcut.TargetPath = Application.ExecutablePath; // path of the executable
+            Shortcut.Save(); // save the shortcut
+
+            SystemAppData = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+            ProductSystemAppDataFolder = SystemAppData + "\\" + Application.ProductName;
+            SystemDir = Directory.CreateDirectory(ProductSystemAppDataFolder);
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(DomainTextBox.Text) || DomainTextBox.Text == "." || DomainTextBox.Text.ToLower() == "localhost")
+            {
+                DomainTextBox.Text = Environment.MachineName;
+            }
+            UserContext = new WrapperImpersonationContext(DomainTextBox.Text, UsernameTextBox.Text, PasswordTextBox.Text);
+            try
+            {
+                UserContext.Enter();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Login attempt failed. Please try again.", "Could not login");
+                UserContext = null;
+                return;
+            }
+
+            SystemClockInFileName = ProductSystemAppDataFolder + "\\" + UserContext.Domain +
+                "-" + UserContext.Username + "-ClockInTimes";
+            SystemClockOutFileName = ProductSystemAppDataFolder + "\\" + UserContext.Domain +
+                "-" + UserContext.Username + "-ClockOutTimes";
+
+            UserAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            ProductUserAppDataFolder = UserAppData + "\\" + Application.ProductName;
+            UserDir = Directory.CreateDirectory(ProductUserAppDataFolder);
+            UserClockInFileName = ProductUserAppDataFolder + "\\ClockInTimes";
+            UserClockOutFileName = ProductUserAppDataFolder + "\\ClockOutTimes";
+
+            UserContext.Leave();
+
+            CopyFileTo(SystemClockInFileName, UserClockInFileName);
+            CopyFileTo(SystemClockOutFileName, UserClockOutFileName);
+
+            if (SessionRadioButton.Checked)
+            {
+                // do session
+                // AppDomain.CurrentDomain.ProcessExit += new EventHandler(OnApplicationExit);
+                // AppDomain.CurrentDomain.DomainUnload += new EventHandler(OnApplicationExit);
+                // Hide();
+                Close();
+            }
+            else if (ClockingInRadioButton.Checked)
+            {
+                SystemClockInFile = OpenFileWriteStream(SystemClockInFileName);
+                UserClockInFile = OpenFileWriteStream(UserClockInFileName);
+                ClockIn();
+                SystemClockInFile.Close();
+                UserClockInFile.Close();
+                Close();
+            }
+            else if (ClockingOutRadioButton.Checked)
+            {
+                SystemClockOutFile = OpenFileWriteStream(SystemClockOutFileName);
+                UserClockOutFile = OpenFileWriteStream(UserClockOutFileName);
+                ClockOut();
+                SystemClockOutFile.Close();
+                UserClockOutFile.Close();
+                Close();
+            }
+
+            
+        }
+
+        private void CancelLoginButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void OnApplicationExit(object sender, EventArgs e)
+        {
+            SystemClockOutFile = OpenFileWriteStream(SystemClockOutFileName);
+            UserClockOutFile = OpenFileWriteStream(UserClockOutFileName);
+            ClockOut();
+            SystemClockOutFile.Close();
+            UserClockOutFile.Close();
+        }
+
+        private void ClockIn()
+        {
+            DateTime now = DateTime.Now;
+            SystemClockInFile.WriteLine(now + " - v" + Assembly.GetExecutingAssembly().GetName().Version);
+            UserClockInFile.WriteLine(now + " - v" + Assembly.GetExecutingAssembly().GetName().Version);
+        }
+
+        private void ClockOut()
+        {
+            DateTime now = DateTime.Now;
+            SystemClockOutFile.WriteLine(now + " - v" + Assembly.GetExecutingAssembly().GetName().Version);
+            UserClockOutFile.WriteLine(now + " - v" + Assembly.GetExecutingAssembly().GetName().Version);
+        }
+
+        private StreamWriter OpenFileReadStream(string FileName, FileMode mode =
+            FileMode.OpenOrCreate, FileAccess access = FileAccess.Read, FileShare share =
+            FileShare.ReadWrite)
+        {
+            FileStream File = new FileStream(FileName, mode, access, share);
+            StreamWriter FileStreamWriter = new StreamWriter(File);
+            return FileStreamWriter;
+        }
+
+        private StreamWriter OpenFileWriteStream(string FileName, FileMode mode =
+            FileMode.Append, FileAccess access = FileAccess.Write, FileShare share =
+            FileShare.Read)
+        {
+            FileStream File = new FileStream(FileName, mode, access, share);
+            StreamWriter FileStreamWriter = new StreamWriter(File);
+            return FileStreamWriter;
+        }
+
+        private void CopyFileTo(string FileName, string DestinationFileName)
+        {
+            FileStream File = new FileStream(FileName, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.Read);
+            FileStream DestFile = new FileStream(DestinationFileName, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.Read);
+            DestFile.SetLength(0);
+            File.CopyTo(DestFile);
+            File.Close();
+            DestFile.Close();
+        }
+    }
+}
